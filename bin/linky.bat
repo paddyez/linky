fd343f9640c27117c0bde257715bd9aa52548fc2
StG1990 <StG1990@aol.com>
2014-04-22 23:05:32 +0200

    Undo of change of factoid plugin, update of xstream library instead, works also

--- bin/linky.bat
+++ bin/linky.bat
@@ -3,4 +3,4 @@ rem run linky
 echo off
 cd "%~dp0%"
 cd ..
-java -cp lib\pircbot-bin.jar;lib\xstream-1.2.2.jar;lib\lib-util.jar;build\classes net.psammead.linky.Linky
+java -cp lib\pircbot-bin.jar;lib\xstream-1.4.7.jar;lib\lib-util.jar;build\classes net.psammead.linky.Linky
